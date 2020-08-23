e07a48b832afdec1938f6ae9e8860058e2b50f58
take-cheeze <takechi101010@gmail.com>
2020-08-23 22:52:23 +0900

    Add serialtool

---
+++ serialtool/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"go.bug.st/serial"
+	"log"
+)
+
+func main() {
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
+	}
+	for _, port := range ports {
+		fmt.Printf("Found port: %v\n", port)
+	}
+
+	mode := &serial.Mode{
+		BaudRate: 115200,
+	}
+	port, err := serial.Open(ports[0], mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buff := make([]byte, 100)
+	for {
+		n, err := port.Read(buff)
+		if err != nil {
+			log.Fatal(err)
+			break
+		}
+		if n == 0 {
+			fmt.Println("\nEOF")
+			break
+		}
+		fmt.Printf("%v", string(buff[:n]))
+	}
+}
